8d33b6e8866eeb8d55ae0db554b92e8b9e6eec3b
43011677 <43011677@sfcollege.edu>
2024-09-05 21:49:49 -0400

    Lesson 1.4 Step Into the Driver's Seat
    Added player control for the vehicle

--- Unit_1-Player Control/PlayerController.cs
+++ Unit_1-Player Control/PlayerController.cs
@@ -9,12 +9,21 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-    public float speed = 20;
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Update is called once per frame
     void Update()
     {
         // We'll move the vehicle forward
         //transform.Translate(0,0,1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
