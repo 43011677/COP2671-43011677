c16425b0ce855dc7cd129b2fd7c603cdbb99e716
43011677 <43011677@sfcollege.edu>
2024-10-03 20:59:41 -0400

    5.3

---
+++ First Project/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    
+    private int score;
+    public TextMeshProUGUI scoreText;    
+    private float spawnRate = 1.0f;
+    void Start()
+    {
+        StartCoroutine(SpawnTarget()); 
+        score = 0;
+        UpdateScore(0);
+    }
+
+    IEnumerator SpawnTarget() {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ First Project/Target.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private GameManager gameManager;
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    private void OnMouseDown() {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+        {
+            return new Vector3(RandomForce().Range(-xRange, xRange), ySpawnPos);
+        }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
